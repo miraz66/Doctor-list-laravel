f7c031fdf9a787369f2c2af3694d82a7f5de862a
Mirajul Islam <mirajulislam0509@gmail.com>
2024-08-13 22:32:19 +0600

    Navbar Active Link

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence(),
+            'salary' => $this->faker->numberBetween(1000, 10000),
+            "description" => $this->faker->paragraph(3),
+        ];
+    }
+}
